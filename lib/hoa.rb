7a0251cf11deba6778d32f7fff685e74fe3c1699
Bryan Rhodunda <rhodunda@udel.edu>
2019-11-17 04:04:26 +0000

    Automatically backed up by Learn

--- lib/hoa.rb
+++ lib/hoa.rb
@@ -6,7 +6,11 @@ BASE_HOA = {
 
 
 def add_character(show, name)
- 
+ BASE_HOA = {
+  :chipmunks => ["Alvin", "Simon", "Theodore"],
+  :third_earthers => ["Lion-O", "Cheetara", "Mumm-Ra (the ever-living)"],
+  :jetsons => ["George", "Jane", "Judy", "Elroy"]
+}
  
  
 
